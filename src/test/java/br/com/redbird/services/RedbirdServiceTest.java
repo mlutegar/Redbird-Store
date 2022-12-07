e04d0ab2187b958195747359c169206f72055339
Michel Lutegar <mlutegar@gmail.com>
2022-12-07 03:32:53 -0300

    update: atualizando o service test

--- src/test/java/br/com/redbird/services/RedbirdServiceTest.java
+++ src/test/java/br/com/redbird/services/RedbirdServiceTest.java
@@ -1,4 +1,102 @@
 package br.com.redbird.services;
 
+import br.com.redbird.domain.model.Roupa;
+import br.com.redbird.repository.RoupaRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
 public class RedbirdServiceTest {
+
+    @Autowired
+    private RoupaRepository roupaRepository;
+
+    @AfterEach
+    void tearDown() {
+        roupaRepository.deleteAll();
+    }
+
+    @Test
+    public void findAll() {
+        var roupa = new Roupa(1L, "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date());
+        roupaRepository.save(roupa);
+
+        var roupaService = new RoupaService(roupaRepository);
+        List<Roupa> roupaList = roupaService.findAll();
+        var lastRoupa = roupaList.get(roupaList.size() - 1);
+
+        assertEquals(roupa.getProductId(), lastRoupa.getProductId());
+        assertEquals(roupa.getNome(), lastRoupa.getNome());
+        assertEquals(roupa.getCor(), lastRoupa.getCor());
+        assertEquals(roupa.getMarca(), lastRoupa.getMarca());
+        assertEquals(roupa.getTamanho(), lastRoupa.getTamanho());
+        assertEquals(roupa.getPreco(), lastRoupa.getPreco());
+        assertEquals(roupa.getQuantidade(), lastRoupa.getQuantidade());
+        assertEquals(roupa.getDataRegistro(), lastRoupa.getDataRegistro());
+        assertEquals(roupa.getDataUltimaAtualizacao(), lastRoupa.getDataUltimaAtualizacao());
+    }
+
+    @Test
+    public void findById() {
+        var roupaSaved = roupaRepository.save(new Roupa(1L, "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date()));
+
+        var roupaService = new RoupaService(roupaRepository);
+        var roupaFinded = roupaService.findById(roupaSaved.getProductId());
+
+        assertEquals(roupaSaved.getProductId(), roupaFinded.getProductId());
+        assertEquals(roupaSaved.getNome(), roupaFinded.getNome());
+        assertEquals(roupaSaved.getCor(), roupaFinded.getCor());
+        assertEquals(roupaSaved.getMarca(), roupaFinded.getMarca());
+        assertEquals(roupaSaved.getTamanho(), roupaFinded.getTamanho());
+        assertEquals(roupaSaved.getPreco(), roupaFinded.getPreco());
+        assertEquals(roupaSaved.getQuantidade(), roupaFinded.getQuantidade());
+        assertEquals(roupaSaved.getDataRegistro(), roupaFinded.getDataRegistro());
+        assertEquals(roupaSaved.getDataUltimaAtualizacao(), roupaFinded.getDataUltimaAtualizacao());
+    }
+
+    @Test
+    public void deleteById(UUID id) {
+        var roupaSaved = roupaRepository.save(new Roupa(1L, "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date()));
+
+        var roupaService = new RoupaService(roupaRepository);
+        var roupaFinded = roupaService.findById(roupaSaved.getProductId());
+
+        assertEquals(roupaSaved.getProductId(), roupaFinded.getProductId());
+        assertEquals(roupaSaved.getNome(), roupaFinded.getNome());
+        assertEquals(roupaSaved.getCor(), roupaFinded.getCor());
+        assertEquals(roupaSaved.getMarca(), roupaFinded.getMarca());
+        assertEquals(roupaSaved.getTamanho(), roupaFinded.getTamanho());
+        assertEquals(roupaSaved.getPreco(), roupaFinded.getPreco());
+        assertEquals(roupaSaved.getQuantidade(), roupaFinded.getQuantidade());
+        assertEquals(roupaSaved.getDataRegistro(), roupaFinded.getDataRegistro());
+        assertEquals(roupaSaved.getDataUltimaAtualizacao(), roupaFinded.getDataUltimaAtualizacao());
+    }
+
+    @Test
+    public void saveRoupa(Roupa roupa) {
+        var roupaService = new RoupaService(roupaRepository);
+
+        roupaService.save(roupa);
+        assertEquals(1.0, roupaRepository.count());
+    }
+
+    @Test
+    void deleteRoupa() {
+        var roupaService = new RoupaService(roupaRepository);
+        var roupa = new Roupa(UUID.randomUUID(), "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date());
+
+        var roupaSaved = roupaService.save(roupa);
+        roupaService.deleteById(roupaSaved.getId());
+
+        assertEquals(0, roupaRepository.count());
+    }
 }
