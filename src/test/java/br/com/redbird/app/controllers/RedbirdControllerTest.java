ba0c051b22083aeda2a71ce2dd0103ec0de65359
Michel Lutegar <mlutegar@gmail.com>
2022-12-11 17:34:08 -0300

    fix: consertando o controllertest

--- src/test/java/br/com/redbird/app/controllers/RedbirdControllerTest.java
+++ src/test/java/br/com/redbird/app/controllers/RedbirdControllerTest.java
@@ -1,29 +1,22 @@
 package br.com.redbird.app.controllers;
 
 import br.com.redbird.domain.model.Roupa;
-import br.com.redbird.services.RedbirdServiceTest;
+import br.com.redbird.services.RoupaService;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import javax.print.attribute.standard.Media;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -31,81 +24,78 @@ import java.util.UUID;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders .*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers .*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-public class RedbirdControllerTest {
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+public class RedBirdControllerTest {
+    @Autowired
+    MockMvc mockMvc;
 
-    @ExtendWith(SpringExtension.class)
-    @WebMvcTest
-    public class RedBirdControllerTest {
-        @Autowired
-        MockMvc mockMvc;
+    @MockBean
+    private RoupaService roupaService;
 
-        @MockBean
-        private RedbirdServiceTest redbirdServiceTest;
+    @Test
+    void getAllRoupas() throws Exception {
+        List<Roupa> roupaList = new ArrayList<>();
+        roupaList.add(new Roupa(UUID.randomUUID(), "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date()));
+        roupaList.add(new Roupa(UUID.randomUUID(), "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date()));
+        Mockito.when(roupaService.findAll()).thenReturn(roupaList);
 
-        @Test
-        void getAllRoupas() throws Exception {
-            List<Roupa> roupaList = new ArrayList<>();
-            roupaList.add(new Roupa(UUID.randomUUID(), "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date()));
-            roupaList.add(new Roupa(UUID.randomUUID(), "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date()));
-            Mockito.when(redbirdServiceTest.findAll()).thenReturn(roupaList);
+        mockMvc.perform(get("/roupas").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", Matchers.hasSize(2))).andDo(print());
+    }
 
-            mockMvc.perform(get("/roupas").contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(jsonPath("$", Matchers.hasSize(2))).andDo(print());
-        }
+    @Test
+    void getRoupa() throws Exception {
+        var roupa = new Roupa(UUID.randomUUID(), "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date());
+        Mockito.when(roupaService.findById(roupa.getProductId())).thenReturn(roupa);
 
-        @Test
-        void getRoupa() throws Exception {
-            var roupa = new Roupa(1L, "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date());
-            Mockito.when(redbirdServiceTest.findById(roupa.getProductId())).thenReturn(roupa);
+        mockMvc.perform(get("/roupa/1" + roupa.getProductId()).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(new ObjectMapper().writeValueAsString(roupa))).andExpect(status().isOk()).andDo(print());
+    }
 
-            mockMvc.perform(get("/roupa/" + roupa.getProductId()).contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(content().json(new ObjectMapper().writeValueAsString(roupa))).andExpect(status().isOk()).andDo(print());
-        }
+    @Test
+    void createRoupa() throws Exception {
+        var roupa = new Roupa(UUID.randomUUID(), "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date());
+        Mockito.when(roupaService.findById(any(UUID.class))).thenReturn(roupa);
 
-        @Test
-        void createRoupa() throws Exception {
-            roupaList.add(new Roupa(1L, "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date()));
-            Mockito.when(roupa.save(any(Roupa.class))).thenReturn(roupa);
-            var mapper = new ObjectMapper();
-            var jsonToDo = mapper.writeValueAsString(roupa);
-
-            var resultActions = mockMvc.perform(post("/roupa").contentType(MediaType.APPLICATION_JSON).content(jsonRoupa));
-            resultActions.andExpect(status().isCreated()).andExpect(content().json(jsonRoupa)).andExpect(jsonPath("$.description").value("Entrar em férias"));
-        }
+        mockMvc.perform(
+                        get("/roupa").contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(content().json(new ObjectMapper().writeValueAsString(roupa), false))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
 
 
-        @Test
-        void editRoupa() throws Exception {
-            var roupa = new Roupa(1L, "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date());
-            Mockito.when(redbirdServiceTest.saveRoupa(any(Roupa.class))).thenReturn(roupa);
+    @Test
+    void editRoupa() throws Exception {
+        var roupa = new Roupa(UUID.randomUUID(), "Vestido", "azul", "marisa", "P", 22.10, 2, new Date(), new Date());
+        Mockito.when(roupaService.saveRoupa(any(Roupa.class))).thenReturn(roupa);
 
-            var mapper = new ObjectMapper();
-            var jsonToDo = mapper.writeValueAsString(roupa);
+        var mapper = new ObjectMapper();
+        var jsonToDo = mapper.writeValueAsString(roupa);
 
-            var resultActions = mockMvc.perform(
-                    post("/roupa")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(jsonToDo)
-            );
+        var resultActions = mockMvc.perform(
+                post("/roupa")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonToDo)
+        );
 
-            resultActions
-                    .andExpect(status().isCreated())
-                    .andExpect(content().json(jsonToDo))
-                    .andExpect(jsonPath("$.nome").value("Vestido"));
-        }
+        resultActions
+                .andExpect(status().isCreated())
+                .andExpect(content().json(jsonToDo))
+                .andExpect(jsonPath("$.nome").value("Vestido"));
+    }
 
-        @Test
-        void deleteToDo() throws Exception {
-            var roupaServiceMock = mock(RedbirdServiceTest.class);
-            doNothing().when(roupaServiceMock).deleteById(any(UUID.class));
+    @Test
+    void deleteToDo() throws Exception {
+        var roupaServiceMock = mock(RoupaService.class);
+        doNothing().when(roupaServiceMock).deleteById(any(UUID.class));
 
-            mockMvc.perform(delete("/roupa/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNoContent());
-
-        }
-
+        mockMvc.perform(delete("/roupa/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNoContent());
     }
 }
