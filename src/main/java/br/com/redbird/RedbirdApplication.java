995ce771c8fd86a24b0b694b9291c888708ec9e2
framgo <lucasaraujooks@gmail.com>
2022-12-13 20:29:00 -0300

    fix: consertando o swagger

--- src/main/java/br/com/redbird/RedbirdApplication.java
+++ src/main/java/br/com/redbird/RedbirdApplication.java
@@ -1,11 +1,7 @@
 package br.com.redbird;
 
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedbirdApplication {
--- src/main/java/br/com/redbird/config/OpenApiConfig.java
+++ src/main/java/br/com/redbird/config/OpenApiConfig.java
@@ -1,17 +1,15 @@
 package br.com.redbird.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 
 @OpenAPIDefinition(
         info = @Info(
-                title = "To Do WebService RestFul",
+                title = "Redbird",
                 version = "1.0.0",
-                description = "To Do Manager",
-                contact = @Contact(name = "Senac Rio", email = "suporte@rj.senac.edu", url = "rj.senac.br")
+                description = "Sistema que gere o cadastro de roupas"
         ),
         servers = {
                 @Server(url = "localhost:8080", description = "Local URL"),
